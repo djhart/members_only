c98e3f2c12636754f07c259863bee34125da7590
dalton <dalton-hart@uiowa.edu>
2017-02-10 02:31:08 +0000

    password digest on user create

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,28 @@
 class User < ApplicationRecord
+  require 'digest/sha1'
+  attr_accessor :remember_token
+  before_validation :remember, on: :create
   validates :name, presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: {case_sensitive: false}
   has_secure_password
+  validates :password, presence: true, length: {minimum: 6}, allow_nil: true
+  # class << self
+  #   def User.new_token
+  #     SecureRandom.urlsafe_base64
+  #   end
+    
+  #   def digest(string)
+  #     Digest::SHA1.hexdigest string
+  #   end
+  # end
+      
+  
+  def remember
+    self.remember_token = SecureRandom.urlsafe_base64
+    update_attribute(:remember_digest, Digest::SHA1.hexdigest(remember_token))
+  end
 end
+
